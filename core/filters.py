86d956b739b5e8718c123fed45e8028c6c5df9d4
Barth <juniior.barth@gmail.com>
2019-05-26 11:42:09 -0300

    Adds Despensa Filters
    close #37

--- core/filters.py
+++ core/filters.py
@@ -1,6 +1,6 @@
 import django_filters
 
-from core.models import Cliente
+from core.models import Cliente, Despensa
 
 
 class ClienteFilter(django_filters.FilterSet):
@@ -8,4 +8,12 @@ class ClienteFilter(django_filters.FilterSet):
 
     class Meta:
         model = Cliente
-        fields = ['nome', ]
+        fields = ['nome', 'login']
+
+
+class DespensaFilter(django_filters.FilterSet):
+    nome = django_filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Despensa
+        fields = ['nome', 'localizacao']
