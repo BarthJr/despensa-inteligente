374da8817637112b9848b361128962b27db2e66a
Barth <juniior.barth@gmail.com>
2019-06-07 21:05:10 -0300

    Refactor cliente dicts

--- core/tests/test_cliente.py
+++ core/tests/test_cliente.py
@@ -5,13 +5,29 @@ from rest_framework.test import APIClient
 from core.models import Cliente, Categoria
 
 
-@pytest.fixture
-def create_cliente():
-    cliente = {
+@pytest.fixture()
+def cliente():
+    return {
         'nome': 'Junior Barth',
         'login': 'barth',
         'senha': 'b07c153de98af7e6ecda7ebf6d1a5e25',
     }
+
+
+
+
+@pytest.fixture()
+def expected_cliente():
+    return {
+        'id': 1,
+        'nome': 'Junior Barth',
+        'login': 'barth',
+        'senha': 'b07c153de98af7e6ecda7ebf6d1a5e25',
+    }
+
+
+@pytest.fixture
+def create_cliente(cliente):
     Cliente.objects.create(**cliente)
 
 
@@ -33,40 +49,20 @@ def test_status_code(client):
 
 
 @pytest.mark.django_db
-def test_get_clientes(create_cliente, client):
-    expected = {
-        'id': 1,
-        'nome': 'Junior Barth',
-        'login': 'barth',
-        'senha': 'b07c153de98af7e6ecda7ebf6d1a5e25',
-    }
+def test_get_clientes(create_cliente, client, expected_cliente):
     resp = client.get('/clientes/')
-    assert resp.data[0] == expected
+    assert resp.data[0] == expected_cliente
 
 
 @pytest.mark.django_db
-def test_post_status_code_clientes(client):
+def test_post_status_code_clientes(client, cliente):
     Categoria.objects.create(nome='Doces')
-    resp = client.post('/clientes/', {
-        'nome': 'Junior Barth',
-        'login': 'barth',
-        'senha': 'b07c153de98af7e6ecda7ebf6d1a5e25',
-    })
+    resp = client.post('/clientes/', cliente)
     assert resp.status_code == status.HTTP_201_CREATED
 
 
 @pytest.mark.django_db
-def test_post_clientes(client):
-    expected = {
-        'id': 1,
-        'nome': 'Junior Barth',
-        'login': 'barth',
-        'senha': 'b07c153de98af7e6ecda7ebf6d1a5e25',
-    }
+def test_post_clientes(client, expected_cliente, cliente):
     Categoria.objects.create(nome='Doces')
-    resp = client.post('/clientes/', {
-        'nome': 'Junior Barth',
-        'login': 'barth',
-        'senha': 'b07c153de98af7e6ecda7ebf6d1a5e25',
-    })
-    assert resp.data == expected
+    resp = client.post('/clientes/', cliente)
+    assert resp.data == expected_cliente
