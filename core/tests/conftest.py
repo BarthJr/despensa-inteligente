efb26842ce2ac7a1e2c991692363ee7daacff8ac
Barth <juniior.barth@gmail.com>
2019-06-07 21:57:35 -0300

    Refactor receita dicts

--- core/tests/conftest.py
+++ core/tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 
+from core.models import Cliente
+
 
 @pytest.fixture()
 def cliente():
@@ -17,4 +19,28 @@ def expected_cliente():
         'nome': 'Junior Barth',
         'login': 'barth',
         'senha': 'b07c153de98af7e6ecda7ebf6d1a5e25',
+    }
+
+
+@pytest.fixture()
+def receita(cliente):
+    clienteObj = Cliente.objects.create(**cliente)
+    return {
+        'titulo': 'Fricasse de Frango',
+        'modoPreparo': 'Desfiar o frango, bater os ingredientes liquidos, refogar no frango, distribuir o queijo',
+        'tempoExecucao': '01:30:00',
+        'quantidade': 4,
+        'cliente': clienteObj
+    }
+
+
+@pytest.fixture()
+def expected_receita(cliente):
+    return {
+        'id': 1,
+        'titulo': 'Fricasse de Frango',
+        'modoPreparo': 'Desfiar o frango, bater os ingredientes liquidos, refogar no frango, distribuir o queijo',
+        'tempoExecucao': '01:30:00',
+        'quantidade': 4,
+        'cliente': 1
     }
