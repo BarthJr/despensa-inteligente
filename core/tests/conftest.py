e55507cf0818eecc54003eef89de80a976432918
Barth <juniior.barth@gmail.com>
2019-06-08 21:24:53 -0300

    Refactor favorita dicts

--- core/tests/conftest.py
+++ core/tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from core.models import Cliente
+from core.models import Cliente, Receita
 
 
 @pytest.fixture()
@@ -42,5 +42,33 @@ def expected_receita(cliente):
         'modoPreparo': 'Desfiar o frango, bater os ingredientes liquidos, refogar no frango, distribuir o queijo',
         'tempoExecucao': '01:30:00',
         'quantidade': 4,
+        'cliente': 1
+    }
+
+
+@pytest.fixture()
+def favoritaObj(receita, cliente):
+    receitaObj = Receita.objects.create(**receita)
+    # clienteObj = Cliente.objects.create(**cliente)
+    return {
+        'receita': receitaObj,
+        'cliente': receitaObj.cliente
+    }
+
+
+@pytest.fixture()
+def favorita(receita, cliente):
+    receita = Receita.objects.create(**receita)
+    return {
+        'receita': receita.pk,
+        'cliente': receita.cliente.pk
+    }
+
+
+@pytest.fixture()
+def expected_favorita(cliente):
+    return {
+        'id': 1,
+        'receita': 1,
         'cliente': 1
     }
--- core/tests/test_favorita.py
+++ core/tests/test_favorita.py
@@ -2,30 +2,12 @@ import pytest
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Favorita, Receita, Cliente
+from core.models import Favorita
 
 
 @pytest.fixture
-def create_favorita():
-    cliente_body = {
-        'nome': 'Junior Barth',
-        'login': 'barth',
-        'senha': 'b07c153de98af7e6ecda7ebf6d1a5e25',
-    }
-    cliente = Cliente.objects.create(**cliente_body)
-    receita_body = {
-        'titulo': 'Fricasse de Frango',
-        'modoPreparo': 'Desfiar o frango, bater os ingredientes liquidos, refogar no frango, distribuir o queijo',
-        'tempoExecucao': '01:30:00',
-        'quantidade': 4,
-        'cliente': cliente
-    }
-    receita = Receita.objects.create(**receita_body)
-    favorita = {
-        'receita': receita,
-        'cliente': cliente
-    }
-    Favorita.objects.create(**favorita)
+def create_favorita(favoritaObj):
+    Favorita.objects.create(**favoritaObj)
 
 
 @pytest.fixture
@@ -46,62 +28,18 @@ def test_status_code(client):
 
 
 @pytest.mark.django_db
-def test_get_favoritas(create_favorita, client):
-    expected = {
-        'id': 1,
-        'receita': 1,
-        'cliente': 1
-    }
+def test_get_favoritas(create_favorita, client, expected_favorita):
     resp = client.get('/favoritas/')
-    assert resp.data[0] == expected
+    assert resp.data[0] == expected_favorita
 
 
 @pytest.mark.django_db
-def test_post_status_code_favoritas(client):
-    cliente_body = {
-        'nome': 'Junior Barth',
-        'login': 'barth',
-        'senha': 'b07c153de98af7e6ecda7ebf6d1a5e25',
-    }
-    cliente = Cliente.objects.create(**cliente_body)
-    receita_body = {
-        'titulo': 'Fricasse de Frango',
-        'modoPreparo': 'Desfiar o frango, bater os ingredientes liquidos, refogar no frango, distribuir o queijo',
-        'tempoExecucao': '01:30:00',
-        'quantidade': 4,
-        'cliente': cliente
-    }
-    Receita.objects.create(**receita_body)
-    resp = client.post('/favoritas/', {
-        'receita': 1,
-        'cliente': 1
-    })
+def test_post_status_code_favoritas(client, favorita):
+    resp = client.post('/favoritas/', favorita)
     assert resp.status_code == status.HTTP_201_CREATED
 
 
 @pytest.mark.django_db
-def test_post_favoritas(client):
-    cliente_body = {
-        'nome': 'Junior Barth',
-        'login': 'barth',
-        'senha': 'b07c153de98af7e6ecda7ebf6d1a5e25',
-    }
-    cliente = Cliente.objects.create(**cliente_body)
-    receita_body = {
-        'titulo': 'Fricasse de Frango',
-        'modoPreparo': 'Desfiar o frango, bater os ingredientes liquidos, refogar no frango, distribuir o queijo',
-        'tempoExecucao': '01:30:00',
-        'quantidade': 4,
-        'cliente': cliente
-    }
-    Receita.objects.create(**receita_body)
-    expected = {
-        'id': 1,
-        'receita': 1,
-        'cliente': 1
-    }
-    resp = client.post('/favoritas/', {
-        'receita': 1,
-        'cliente': 1
-    })
-    assert resp.data == expected
+def test_post_favoritas(client, favorita, expected_favorita):
+    resp = client.post('/favoritas/', favorita)
+    assert resp.data == expected_favorita
